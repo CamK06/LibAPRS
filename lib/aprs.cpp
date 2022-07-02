cbf8bf7342666316ce8c6e069a561a5b97ef72d4
Cam K <a@a>
2022-07-02 02:00:36 -0400

    Set up buildsystem + basic project structure

---
+++ lib/aprs.cpp
@@ -0,0 +1,6 @@
+#include "libaprs/aprs.h"
+
+void libAPRSTest()
+{
+    std::cout << "Test! from libAPRS" << std::endl;
+}
---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "libaprs/aprs.h"
+
+int main()
+{
+    std::cout << "APRSLib test program" << std::endl;
+    libAPRSTest();
+}
